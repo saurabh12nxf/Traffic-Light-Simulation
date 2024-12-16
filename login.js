274bd7455df89d0137be8eead2af09cb3ec7e776
saurabh12nxf <sureshsingh09120@gmail.com>
2024-12-16 16:16:24 +0530

    Added feature

--- login.js
+++ login.js
@@ -10,7 +10,7 @@ function login() {
         loginMessage.style.color = "green";
         loginMessage.textContent = "Login successful! Redirecting...";
         setTimeout(() => {
-            window.location.href = "traffic-light.html"; // Redirect to traffic light page
+            window.location.href = "index.html"; // Redirect to traffic light page
         }, 2000);
     } else {
         loginMessage.style.color = "red";
